12642e53ba1b5d68509d0eb71270f244b7a5a8de
IntelSDM <GitBackup@shibemaster.me>
2023-08-26 00:02:28 +0100

    Revert "Debug Garbage Collection"
    This reverts commit 3a48795b8bb2373c9b007e5e6e8d2dbff45d681e.

--- 7DTD/Configs/Config.cs
+++ 7DTD/Configs/Config.cs
@@ -16,6 +16,5 @@ namespace Cheat.Configs
         public Aimbot Aimbot = new Aimbot();
         public Friends Friends = new Friends();
         public Tiles Tiles = new Tiles();
-        public Debug Debug = new Debug();
     }
 }
